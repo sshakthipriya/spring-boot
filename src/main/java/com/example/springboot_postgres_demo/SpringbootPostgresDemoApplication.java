ce4ce45f055282587d349539635ae051c1dc1231
sshakthipriya <101383645+sshakthipriya@users.noreply.github.com>
2025-11-06 19:22:21 +0530

    Add files via upload

---
+++ src/main/java/com/example/springboot_postgres_demo/SpringbootPostgresDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_postgres_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgresDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_postgres_demo/SpringbootPostgresDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_postgres_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
